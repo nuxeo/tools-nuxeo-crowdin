0464f68888b5f070383e77f6814373c2f7594956
Anahide Tchertchian <at@nuxeo.com>
2017-01-05 19:03:45 +0100

    NXBT-1485/NXP-21385: remove extra checkout of media-publishing for Crowdin

--- jenkins/check_nuxeo_crowdin_sync.sh
+++ jenkins/check_nuxeo_crowdin_sync.sh
@@ -51,7 +51,6 @@ cd addons
 # NXBT-1381: extra clone disconnected addons for 8.10
 git clone git@github.com:nuxeo/nuxeo-agenda.git
 git clone git@github.com:nuxeo/nuxeo-groups-rights-audit.git
-git clone git@github.com:nuxeo/nuxeo-media-publishing.git
 git clone git@github.com:nuxeo/nuxeo-multi-tenant.git
 git clone git@github.com:nuxeo/nuxeo-platform-spreadsheet.git
 git clone git@github.com:nuxeo/nuxeo-travel-expenses.git
