214eb3b492b7ba2d7a827e67a77c1c179fbffa27
Anahide Tchertchian <at@nuxeo.com>
2017-02-21 18:13:55 +0100

    NXBT-1485/NXP-21387: remove extra checkout of nuxeo-platform-spreadsheet for Crowdin

--- jenkins/check_nuxeo_crowdin_sync.sh
+++ jenkins/check_nuxeo_crowdin_sync.sh
@@ -50,7 +50,6 @@ cd $NUXEO_PATH
 cd addons
 # NXBT-1381: extra clone disconnected addons for 8.10
 git clone git@github.com:nuxeo/nuxeo-groups-rights-audit.git
-git clone git@github.com:nuxeo/nuxeo-platform-spreadsheet.git
 git clone git@github.com:nuxeo/nuxeo-travel-expenses.git
 cd $HERE
 
